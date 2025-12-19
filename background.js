e181ce6ceff15a5cd0aa1afb09f6b2ca1976e854
Transwarp8 <transwarp8@users.noreply.github.com>
2025-12-19 16:00:23 -0500

    Remove storage permission

--- background.js
+++ background.js
@@ -1,35 +1,3 @@
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === 'install') {
-    chrome.storage.local.set({
-      minFreq: 200,
-      maxFreq: 3000,
-      minScore: 10,
-      ldpcIterations: 25,
-      autoStart: false
-    });
-  }
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.type) {
-    case 'GET_SETTINGS':
-      chrome.storage.local.get(null, (settings) => {
-        sendResponse(settings);
-      });
-      return true;
-      
-    case 'SAVE_SETTINGS':
-      chrome.storage.local.set(message.settings, () => {
-        sendResponse({ success: true });
-      });
-      return true;
-      
-    case 'LOG':
-      sendResponse({ received: true });
-      break;
-  }
-});
-
 let keepAliveInterval = null;
 
 chrome.runtime.onConnect.addListener((port) => {
